ea335470eaca3b5c1fd4fa93a597eb44b1d16e1f
0t4u <61939142+0t4u@users.noreply.github.com>
2021-02-01 09:41:52 +0800

    un-callback twist
    imagine

--- sources/twist.js
+++ sources/twist.js
@@ -13,12 +13,12 @@ class TwistMoe {
      * @param {string} animeToSearchFor anime name
      * @param {function} cb callback
      */
-    async search(animeToSearchFor, cb) {
+    async search(animeToSearchFor) {
         const link = `${this.base}/anime`;
         const res = JSON.parse(await (await fetch(link, {headers:{'x-access-token': '0df14814b9e590a1f26d3071a4ed7974', 'user-agent': '1.0.16'}})).text());
         const anime = animeToSearchFor.toLowerCase();
         const f = res.find(x => x.slug.slug === anime || x.title.toLowerCase() === anime) || res.find(x => x.slug.slug.includes(anime) || x.title.toLowerCase().includes(anime));
-        cb([f])
+        return f
     }
     /**
      * Get download link
@@ -26,17 +26,17 @@ class TwistMoe {
      * @param {string} ep episode
      * @param {function} cb callback
      */
-    async getvideo(animeToSearchFor, ep, cb) {
-        this.search(animeToSearchFor, ep, (async (a) => {
-            const ani = a[0]
+    async getvideo(animeToSearchFor, ep) {
+        this.search(animeToSearchFor, ep).then(async (a) => {
+            const ani = a
             const l2 = `${this.base}/anime/${ani.slug.slug}/sources`;
             const r2 = JSON.parse(await (await fetch(l2, {headers:{'x-access-token': '0df14814b9e590a1f26d3071a4ed7974', 'user-agent': '1.0.16'}})).text());
             const b2 = r2.find(x => x.number === parseInt(ep));
             const e2 = b2.source;
             const c2 = crypto.AES.decrypt(e2, '267041df55ca2b36f2e322d05ee2c9cf').toString(crypto.enc.Utf8);
             const r3 = `https://cdn.twist.moe${c2}`
-            cb(r3)
-        }))
+            return r3
+        })
     }
     /**
      * Download an anime
@@ -45,7 +45,7 @@ class TwistMoe {
      * @param {string} dir directory
      */
     async download(animeToSearchFor, ep, dir) {
-        this.getvideo(animeToSearchFor, ep, (async (ani) => {
+        this.getvideo(animeToSearchFor, ep).then(async (ani) => {
             const file = fs.createWriteStream(dir);
             fetch(ani, { headers: { 'user-agent': '1.0.16', 'referer': this.base } }).then(res => {
                 if(!res.ok) throw new Error(`Server responded with ${res.status} (${res.statusText})`)
@@ -54,7 +54,7 @@ class TwistMoe {
             }).catch(ex => {
                 throw new Error(ex)
             })
-        }))
+        })
     }
 }
 
